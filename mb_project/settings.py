19eaa38141ecef3c7c98cacb70865a95035441b2
AmanGiri007 <giriaman437@gmail.com>
2021-07-03 21:52:23 +0545

    change

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l^l#zfgarslk1^p^r6!xe@)71f+khhs=^jw!b#0ww0q(sk4bgf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
